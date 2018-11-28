82ab3dee50d5197594dcd4dd7c3a8a5367eb2400
darius <dariusw@mastersystem.com>
2018-11-28 14:25:54 -0800

    bootcamp cont...

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -22,14 +22,36 @@ class Node {
     }
 
     remove(data) {
-        const node = new Node(data);
-        this.children.pop(node);
+        this.children = this.children.filter(node => {
+            return node.data !== data;
+        });
     }
 }
 
 class Tree {
     constructor() {
         this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift();
+
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift();
+            arr.unshift(...node.children);
+            fn(node);
+        }
     }
 }
 
